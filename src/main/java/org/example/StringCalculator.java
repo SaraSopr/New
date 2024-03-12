72d13c916e95da4d12e37ed38b3169aaf3262a30
sara.soprana <sarasoprana12@hotmail.it>
2024-03-12 11:12:59 +0100

    Refactor

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -7,12 +7,14 @@ public class StringCalculator {
     public String add(String number) {
         if (number.isEmpty())
             return "0";
+        return getSum(number);
+    }
+
+    private static String getSum(String number) {
         String[] temp = number.split(",");
         BigDecimal sum = BigDecimal.ZERO;
-        for (String s : temp) {
+        for (String s : temp)
             sum = sum.add(new BigDecimal(s));
-        }
-
         return sum.toString();
     }
 }
